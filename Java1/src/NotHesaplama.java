2f7998965c9665dff42d957a7941a56f5303f75d
sinanalpay <sinan.alpay@hotmail.com>
2023-10-01 20:41:20 +0300

    Not hesaplama verileri girildi

--- Java1/src/NotHesaplama.java
+++ Java1/src/NotHesaplama.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class NotHesaplama {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		int quiz , vize , ffinal;
+		double ortalama ;
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("quiz notunu giriniz : ");
+		quiz = input.nextInt();
+		
+		System.out.println("vize notunu giriniz : ");
+		vize = input.nextInt();
+		
+		System.out.println("final notunu giriniz : ");
+		ffinal = input.nextInt();
+		
+		ortalama = (quiz * 0.2) + (vize * 0.5) + (ffinal * 0.3);
+		System.out.println("not ortalamanız : " + ortalama);
+		
+		String sonuc = (ortalama >= 50) ? "Başarılı" : "Başarısız";
+		System.out.println(sonuc);
 
 	}
 
